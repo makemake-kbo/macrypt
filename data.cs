ee486390b91db3ad4b424b10253672e51e1911a8
Vukasin Gostovic <vukasin@gostovic.de>
2021-05-13 09:20:15 +0200

    added data types file

---
+++ data.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace macrypt {
+
+    public class transaction
+    {
+
+        public transaction() { }
+        public transaction(string from, string to, int amount)
+        {
+            this.From = from;
+            this.To = to;
+            this.Amount = amount;
+        }
+
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class block {
+        public ulong nonce { get; set; }
+        public string hash { get; set; }
+        public string previousHash { get; set; }
+        public DateTime timestamp { get; set; }
+        public List<transaction> txList { get; set; }
+    }
+
+}
