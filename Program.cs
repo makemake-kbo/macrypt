3321ca095744d3657ca29a3ec8dbb0cdea274a2e
Vukasin Gostovic <vukasin@gostovic.de>
2021-05-13 08:48:40 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace macrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
