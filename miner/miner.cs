4ee704d8e5b46f8523c02c1720e8ba0169b2553c
Vukasin Gostovic <vukasin@gostovic.de>
2021-05-20 09:14:54 +0200

    added createRootHash for creating merkle tree roots

--- miner/miner.cs
+++ miner/miner.cs
@@ -44,6 +44,29 @@ namespace macrypt.Miner {
 
         }
 
+        public string createRootHash(IList<string> merkelLeaves) {
+            if (merkelLeaves == null || !merkelLeaves.Any()) {
+                return string.Empty;
+            }
+            if (merkelLeaves.Count() == 1) {
+                return merkelLeaves.First();
+            }
+
+            if (merkelLeaves.Count() % 2 > 0) {
+                merkelLeaves.Add(merkelLeaves.Last());
+            }
+
+            var merkleBranches = new List<string>();
+
+            for (int i = 0; i < merkelLeaves.Count(); i += 2)
+            {
+                var leafPair = string.Concat(merkelLeaves[i], merkelLeaves[i + 1]);
+                merkleBranches.Add(calculateHash(calculateHash(leafPair)));
+            }
+
+            return createRootHash(merkleBranches);
+        }
+
         public static string calculateHash(string rawData)
         {
             // SHA512 for asiic resistance hehe
